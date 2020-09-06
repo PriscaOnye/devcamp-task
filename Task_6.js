6a8a392c3ae8b5c27e9e0bdd6d97006a7545c980
PriscaOnyee <70857042+PriscaOnyee@users.noreply.github.com>
2020-09-06 20:02:24 +0100

    Create Task_6.js

---
+++ Task_6.js
@@ -0,0 +1,34 @@
+function GetThreeAddition(array, sum) {
+  // Loop through to get the 1st value
+  for (let i = 0; i < array.length; i++) {
+    // Loop through to get the 2nd value
+    for (let i2 = 0; i2 < array.length; i2++) {
+      // Loop through to get the 3rd value
+      for (let i3 = 0; i3 < array.length; i3++) {
+        // set val1
+        var val1 = array[i];
+
+        // Check if it's a duplicate index
+        if ( i != i2 ){
+          // set val2
+          var val2 = array[i2];
+        }
+
+        // Check if it's not a duplicate index
+        if ( i != i2 && i != i3 && i2 != i3) {
+          // set val3
+          var val3 = array[i3];
+        }
+
+        // Check if addition of the 3 values add up to the sum
+        if((val1 + val2 + val3) == sum){
+          // Return the values
+          return [val1, val2, val3];
+        }
+      }
+    }
+  }
+}
+
+// Testing
+console.log(GetThreeAddition([1,2,3,4,5,6], 6));
